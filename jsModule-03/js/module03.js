a9a2a11d7030954a8ebe82bde398c3eb691a6d2f
bilcha <ivanova.olenk@gmail.com>
2018-03-05 21:42:59 +0200

    upload JsModule-03

---
+++ jsModule-03/js/module03.js
@@ -0,0 +1,13 @@
+const alphabet = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+
+const aArray = alphabet.split("")
+
+
+const firstRow = aArray.slice(0, aArray.indexOf(']'));
+const secondRow = aArray.slice(aArray.indexOf('a'), aArray.indexOf("'"));
+const thirdRow = aArray.slice(aArray.indexOf('z'));
+
+const keyboard = [firstRow, secondRow, thirdRow];
+console.log(`${keyboard[1][5]}${keyboard[0][2]}${keyboard[1][8]}${keyboard[1][8]}${keyboard[0][8]}`);
+console.log(`${keyboard[1][6]}${keyboard[1][0]}${keyboard[2][3]}${keyboard[1][0]}${keyboard[1][1]}${keyboard[2][2]}${keyboard[0][3]}${keyboard[0][7]}${keyboard[0][9]}${keyboard[0][4]}`);
+console.log(`${keyboard[0][4]}${keyboard[0][3]}${keyboard[1][0]}${keyboard[0][7]}${keyboard[2][5]}${keyboard[0][2]}${keyboard[0][3]}`);
