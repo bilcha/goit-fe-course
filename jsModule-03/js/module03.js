0f35888666089a1d766425a8a38addd7c7719d91
bilcha <ivanova.olenk@gmail.com>
2018-03-06 22:51:06 +0200

    add console.log in module-03

--- jsModule-03/js/module03.js
+++ jsModule-03/js/module03.js
@@ -9,18 +9,10 @@ const thirdRow = aArray.slice(aArray.indexOf('z'));
 
 const keyboard = [firstRow, secondRow, thirdRow];
 
-// если отдельная переменная для каждого слова
-let resHello = (`${keyboard[1][5]}${keyboard[0][2]}${keyboard[1][8]}${keyboard[1][8]}${keyboard[0][8]}`);
-let resScript = (`${keyboard[1][6]}${keyboard[1][0]}${keyboard[2][3]}${keyboard[1][0]}${keyboard[1][1]}${keyboard[2][2]}${keyboard[0][3]}${keyboard[0][7]}${keyboard[0][9]}${keyboard[0][4]}`);
-let resTrainer = (`${keyboard[0][4]}${keyboard[0][3]}${keyboard[1][0]}${keyboard[0][7]}${keyboard[2][5]}${keyboard[0][2]}${keyboard[0][3]}`);
 
-// переиспользование переменной
-
-// let result = (`${keyboard[1][5]}${keyboard[0][2]}${keyboard[1][8]}${keyboard[1][8]}${keyboard[0][8]}`);
-// console.log(result)
-
-// result = (`${keyboard[1][6]}${keyboard[1][0]}${keyboard[2][3]}${keyboard[1][0]}${keyboard[1][1]}${keyboard[2][2]}${keyboard[0][3]}${keyboard[0][7]}${keyboard[0][9]}${keyboard[0][4]}`);
-// console.log(result)
-
-// result = (`${keyboard[0][4]}${keyboard[0][3]}${keyboard[1][0]}${keyboard[0][7]}${keyboard[2][5]}${keyboard[0][2]}${keyboard[0][3]}`);
-// console.log(result)
+const resHello = `${keyboard[1][5]}${keyboard[0][2]}${keyboard[1][8]}${keyboard[1][8]}${keyboard[0][8]}`;
+console.log (resHello);
+const resScript = `${keyboard[1][6]}${keyboard[1][0]}${keyboard[2][3]}${keyboard[1][0]}${keyboard[1][1]}${keyboard[2][2]}${keyboard[0][3]}${keyboard[0][7]}${keyboard[0][9]}${keyboard[0][4]}`;
+console.log (resScript);
+const resTrainer = `${keyboard[0][4]}${keyboard[0][3]}${keyboard[1][0]}${keyboard[0][7]}${keyboard[2][5]}${keyboard[0][2]}${keyboard[0][3]}`;
+console.log (resTrainer);
