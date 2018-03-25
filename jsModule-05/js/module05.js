91b124e8e4d843a35d84f5ae92ee8af97ac33ac9
bilcha <ivanova.olenk@gmail.com>
2018-03-25 23:03:31 +0300

    fix module-05

--- jsModule-05/js/module05.js
+++ jsModule-05/js/module05.js
@@ -1,40 +1,36 @@
 const alphabethEn = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
 const alphabethRu = "йцукенгшщзхъфывапролджэячсмитьбю.";
 const alphabethUa = "йцукенгшщзхїфівапролджєячсмитьбю.";
+const layoutRows = ["topRow", "middleRow", "bottomRow"];
 
-const addKeyboardLayout = (stringAlfabet, lineBrOne, lineBrTwo) => {
+
+const createKeyboardLayout = (stringAlfabet, lineBrOne, lineBrTwo) => {
 const aArray = stringAlfabet.split("");
 return keyboardAr = [aArray.slice(0, aArray.indexOf(lineBrOne)), aArray.slice(aArray.indexOf(lineBrOne), aArray.indexOf(lineBrTwo)), aArray.slice(aArray.indexOf(lineBrTwo))];
-}
-const alphabetArrEn = addKeyboardLayout (alphabethEn, "a", "z");
-const alphabetArrRu = addKeyboardLayout (alphabethRu, "ф", "я");
-const alphabetArrUa = addKeyboardLayout (alphabethUa, "ф", "я");
-      
-const keyboard = {
-    layouts: {
-        en: {
-            topRow: alphabetArrEn[0],
-            middleRow: alphabetArrEn[1],
-            bottomRow: alphabetArrEn[2],
-        },
-        ru: {
-            topRow: alphabetArrRu[0],
-            middleRow: alphabetArrRu[1],
-            bottomRow: alphabetArrRu[2],
-        },
-        ua: {
-            topRow: alphabetArrUa[0],
-            middleRow: alphabetArrUa[1],
-            bottomRow: alphabetArrUa[2],
-        }
-    },
-    langs: ['en', 'ru', 'ua'],
-    currentLang: ''
 };
+
+const keyboard = {
+  layouts: {},
+  langs: [],
+  currentLang: '',
+  addLayout: function (layout, lang) {
+    this.layouts[lang] = {};
+    this.layouts[lang][layoutRows[0]] = layout[0];
+    this.layouts[lang][layoutRows[1]] = layout[1]; 
+    this.layouts[lang][layoutRows[2]] = layout[2]; 
+    if (this.langs.indexOf(lang) < 0){
+      this.langs.push(lang);
+    }
+  }
+}
+keyboard.addLayout(createKeyboardLayout(alphabethEn, "a", "z"), "en");
+keyboard.addLayout(createKeyboardLayout(alphabethRu, "ф", "я"), "ru");
+keyboard.addLayout(createKeyboardLayout(alphabethUa, "ф", "я"), "ua");
 
 doPrompt = function () {
   let langChoice = prompt ("Выберете язык  en-0, ru-1, ua-2", 0);
   switch (langChoice) {
+    case null: break;
     case "0":
       alert("You choose english");  
       keyboard.currentLang = keyboard.langs[0];
@@ -55,17 +51,21 @@ doPrompt = function () {
 
 doPrompt();
 
+
 // проврка
 console.log(keyboard.currentLang);
 
 let path = "layouts." + keyboard.currentLang;
 let getCurrentLang = path.split('.').reduce(function(prev, curr) {       
         return prev ? prev[curr] : undefined}, keyboard);
+// проверка
+console.log(getCurrentLang);
 
 const getRand = arr => Math.floor(Math.random() * arr.length);
 
-const getRandCharInAlph = (arr) => {
-let arrayIdx = getRand(arr);
-return arr[arrayIdx][getRand(arr[arrayIdx])];
+const getRandCharInAlph = (langObj) => {
+  let layoutRow = langObj[layoutRows[getRand(layoutRows)]]
+  return layoutRow[getRand(layoutRow)];
 }
-console.log(getRandCharInAlph(Object.values(getCurrentLang)));
+// проверка
+console.log(getRandCharInAlph(getCurrentLang));
